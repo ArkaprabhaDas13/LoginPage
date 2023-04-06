b350591433954d175348c78061450e0436497cd8
ArkaprabhaDas13 <arkaprabha31@gmail.com>
2023-04-06 22:51:55 +0530

    fifth commit

--- src/Register.jsx
+++ src/Register.jsx
@@ -20,7 +20,7 @@ export const Register = () => {
         <div className='form'>
                 
             <label htmlFor="text">Username</label>
-            <input value={username} type="text" placeholder='Username' onChange={(e)=>setPassword(e.target.value)} id='name' />
+            <input value={username} type="text" placeholder='Username' onChange={(e)=>setName(e.target.value)} id='name' />
 
             <label htmlFor="email">Email</label>
             <input value={email} type="text" placeholder='Your Email' id='email' name='email' onChange={(e)=>setEmail(e.target.value)}/>
